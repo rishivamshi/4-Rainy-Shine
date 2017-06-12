b9c5eedba6cd11cafa11331c66392de1496bda83
Rishi <Rishi@rishis-MacBook-Pro.local>
2017-06-13 04:17:12 +0530

    Previous forgot - Table View Cells Prototype finished. Current commit is about the constants and the currentweather.swift files. It takes the url from openweathermap and creates variables. no. 78

---
+++ RainyShine/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  RainyShine
+//
+//  Created by Rishi on 13/06/17.
+//  Copyright © 2017 Rishi. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "59fcf70c8f597d1fc023a7eab6609beb"
+
+
---
+++ RainyShine/CurrentWeather.swift
@@ -0,0 +1,58 @@
+//
+//  CurrentWeather.swift
+//  RainyShine
+//
+//  Created by Rishi on 13/06/17.
+//  Copyright © 2017 Rishi. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+class CurrentWeather {
+    
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+    
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+        
+        
+    }
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+        
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    
+    var currentTemp: Double {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+    
+}
